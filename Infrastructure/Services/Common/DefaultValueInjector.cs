0e2af1a3b112b325f5982707c54ed2690f8bd4c2
rissh <rissh@TIHAM_PC>
2024-02-09 00:28:53 +0600

    add default value injector

---
+++ Infrastructure/Services/Common/DefaultValueInjector.cs
@@ -0,0 +1,69 @@
+﻿using Persistence.Authentication.CurrentUserContext;
+using Persistence.Tenant;
+
+namespace Infrastructure.Services.Common;
+
+public class DefaultValueInjector
+{
+    private readonly TenantProvider _tenantProvider;
+    private readonly Guid _tenantId;
+    private readonly CurrentUser _currentUser;
+    public DefaultValueInjector(TenantProvider tenantProvider, ICurrentUserProvider currentUserProvider)
+    {
+        _tenantProvider = tenantProvider;
+        _tenantId = _tenantProvider.GetTenantId();
+        _currentUser = currentUserProvider.GetCurrentUser();
+    }
+
+    public T InjectTenant<T, Key>(T model) where T : BaseEntity<Key>
+    {
+        model.TenantId = _tenantId;
+        return model;
+    }
+
+    public T InjectCreatingAudit<T, Key>(T model) where T : AuditableEntity<Key>
+    {
+        model.TenantId = _tenantId;
+        if (model.CreatedById == 0)
+            model.CreatedById = _currentUser.Id;
+        model.CreatedTime = DateTime.UtcNow;
+
+        return model;
+    }
+
+    public T InjectUpdatingAudit<T, Key>(T model) where T : AuditableEntity<Key>
+    {
+        model.TenantId = _tenantId;
+        model.LastUpdatedById = _currentUser.Id;
+        model.LastUpdatedTime = DateTime.UtcNow;
+
+        return model;
+    }
+
+    public List<T> InjectTenant<T, Key>(List<T> entities) where T : BaseEntity<Key>
+    {
+        foreach (var entity in entities)
+        {
+            InjectTenant<T, Key>(entity);
+        }
+        return entities;
+    }
+
+    public List<T> InjectCreatingAudit<T, Key>(List<T> entities) where T : AuditableEntity<Key>
+    {
+        foreach (var entity in entities)
+        {
+            InjectCreatingAudit<T, Key>(entity);
+        }
+        return entities;
+    }
+
+    public List<T> InjectUpdatingAudit<T, Key>(List<T> entities) where T : AuditableEntity<Key>
+    {
+        foreach (var entity in entities)
+        {
+            InjectUpdatingAudit<T, Key>(entity);
+        }
+        return entities;
+    }
+}
